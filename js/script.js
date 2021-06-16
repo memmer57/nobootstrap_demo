88af38ac1856859b932e2b13133a6f4b9877592b
memmer57 <matej.memmer@gmail.com>
2021-06-16 18:13:11 +0200

    final commit done

--- js/script.js
+++ js/script.js
@@ -1,4 +1,3 @@
-/* Toggle between showing and hiding the navigation menu links when the user clicks on the hamburger menu / bar icon */
 function open_navbar() {
     var x = document.getElementById("items");
     if (x.style.display === "block") {
